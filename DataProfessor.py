69524589a93df0f46ca9754ba6458e4127175bee
Cher-rylllll <109711152+Cher-rylllll@users.noreply.github.com>
2022-07-21 08:46:20 +0100

    Update DataProfessor.py

--- DataProfessor.py
+++ DataProfessor.py
@@ -39,6 +39,8 @@ prediction_proba = clf.predict_proba(df)
 st.subheader('Class labels and their corresponding index number')
 st.write(['Iris-Setosa','Iris-Versicolor','Iris-Virginica'])
 
+column_names = ["Iris-Setosa", "Iris-Versicolo", "Iris-Virginica"]
+
 st.subheader('Prediction')
 st.write(prediction)
 
